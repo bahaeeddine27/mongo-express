dc9a6ad3948b041681341950e8dbc09ee936ceab
Bahaeeddine27 <kiliminelbooking@gmail.com>
2024-04-17 11:18:12 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+require ('./app/model/index.js')
+
+const app = express()
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const app = require("./app.js");
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
